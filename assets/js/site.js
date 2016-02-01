808418f6bd2ce025e17d60688fdcabfa276899ab
Cryptkeeper <hello@cryptkpr.me>
2016-02-01 05:31:00 -0600

    Correctly init these

--- assets/js/site.js
+++ assets/js/site.js
@@ -282,6 +282,9 @@ $(document).ready(function() {
         if (shownServers) {
             var keys = Object.keys(rawData);
 
+            hiddenGraphData = [];
+            displayedGraphData = [];
+
             for (var i = 0; i < keys.length; i++) {
                 var name = keys[i];
 
