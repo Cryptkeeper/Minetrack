f8ce3ac7f8d750eb4086eaa247775f0b0771c8cc
Nick Krecklow <hello@github.nklow.com>
2020-04-18 23:45:58 -0500

    merge #settings-toggle, #sort-by and .mojang-status CSS

--- assets/js/mojang.js
+++ assets/js/mojang.js
@@ -1,14 +1,16 @@
+const MOJANG_STATUS_BASE_CLASS = 'header-button header-button-group'
+
 export class MojangUpdater {
   updateServiceStatus (status) {
     // HACK: ensure mojang-status is added for alignment, replace existing class to swap status color
-    document.getElementById('mojang-status_' + status.name).setAttribute('class', 'mojang-status mojang-status-' + status.title.toLowerCase())
+    document.getElementById('mojang-status_' + status.name).setAttribute('class', MOJANG_STATUS_BASE_CLASS + ' mojang-status-' + status.title.toLowerCase())
     document.getElementById('mojang-status-text_' + status.name).innerText = status.title
   }
 
   reset () {
     // Strip any mojang-status-* color classes from all mojang-status classes
     document.querySelectorAll('.mojang-status').forEach(function (element) {
-      element.setAttribute('class', 'mojang-status')
+      element.setAttribute('class', MOJANG_STATUS_BASE_CLASS)
     })
 
     document.querySelectorAll('.mojang-status-text').forEach(function (element) {
