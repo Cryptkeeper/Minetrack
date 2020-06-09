002957391b01f1916ae0969251ecfcece412ea24
Nick Krecklow <hello@github.nklow.com>
2020-06-08 19:35:38 -0500

    Add popup alert for data.minetrack.me

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,13 @@ import { App } from './app'
 
 const app = new App()
 
+function dismissAlert () {
+  document.getElementById('alert').style.display = 'none'
+  if (typeof localStorage !== 'undefined') {
+    localStorage.setItem('minetrack_alert_dismissed', true)
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   app.init()
 
@@ -11,4 +18,19 @@ document.addEventListener('DOMContentLoaded', () => {
     // Delegate to GraphDisplayManager which can check if the resize is necessary
     app.graphDisplayManager.requestResize()
   }, false)
+
+  document.getElementById('alert-dismiss').addEventListener('click', () => {
+    dismissAlert()
+  })
+
+  if (typeof localStorage !== 'undefined') {
+    const isDismissed = localStorage.getItem('minetrack_alert_dismissed') !== null
+    if (!isDismissed) {
+      document.getElementById('alert').style.display = 'block'
+
+      setTimeout(() => {
+        dismissAlert()
+      }, 30 * 1000)
+    }
+  }
 }, false)
